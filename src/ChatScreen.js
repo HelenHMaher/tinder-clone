04164ee67a3b8d0a79b3c25b6bf0fa3a7bc3b03e
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-31 16:14:56 +0100

    basic build complete

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -3,6 +3,7 @@ import "./ChatScreen.css";
 import Avatar from "@material-ui/core/Avatar";
 
 function ChatScreen() {
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([
     {
       name: "Ellen",
@@ -18,6 +19,12 @@ function ChatScreen() {
     },
     { message: "Hey, pretty good!" },
   ]);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
 
   return (
     <div className="chatScreen">
@@ -40,6 +47,23 @@ function ChatScreen() {
           </div>
         )
       )}
+
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          type="text"
+          placeholder="type a message..."
+        />
+        <button
+          onClick={handleSend}
+          type="submit"
+          className="chatScreen__inputButton"
+        >
+          SEND
+        </button>
+      </form>
     </div>
   );
 }
