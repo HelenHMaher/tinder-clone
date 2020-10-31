aca2613b101f3f9b80b56696a2eedbb09019f71e
HelenHMaher <helenharlanmaher@gmail.com>
2020-10-31 10:18:17 +0100

    1:15:45 of walk-through

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chatpage</h1>
+          </Route>
+          <TinderCards />
+          <Route path="/">
+            <h1>I am the homepage</h1>
+          </Route>
+        </Switch>
+        {/* Tidner cards */}
+        {/* Buttons below tinder cards */}
+
+        {/*Chats screen */}
+        {/* Individual chat screen */}
+      </Router>
     </div>
   );
 }
